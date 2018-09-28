56f717821bf1a4cba99e78ec100e8b29a4e62a3c
Marília Gabriele Suzart Silva <gabrielesuzart@gmail.com>
2018-09-28 19:04:45 -0300

    Seção 3 - Lição 35. Directives - Lista funcionando

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,13 +3,17 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>' 
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        ` 
 })
 
 export class CoursesComponent {
-     title = "Lista de cursos";
-
-     getTitle() {
-        return this.title;
-     }
+    title = "Lista de cursos";
+    courses = ["curso1", "curso2", "curso3"];
 }
