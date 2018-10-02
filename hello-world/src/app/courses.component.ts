d2de7453e79ad9570426d4e3250159da4289745d
Marília Gabriele Suzart Silva <gabrielesuzart@gmail.com>
2018-10-02 17:29:03 -0300

    Section 4, Lecture 40

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,19 +1,28 @@
 
 import {Component} from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
         <h2>{{ title }}</h2>
+        <h2 [textContent]="title"></h2>
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
             </li>
         </ul>
+        <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl" />
         ` 
 })
 
 export class CoursesComponent {
     title = "Lista de cursos";
+    imageUrl = "http://lorempixel.com/400/200";
     courses;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 }
--- hello-world/src/app/courses.service.ts
+++ hello-world/src/app/courses.service.ts
@@ -1,5 +1,5 @@
 
-export class CousesService {
+export class CoursesService {
     getCourses() {
         return ["curso1", "curso2", "curso3"];
     }
